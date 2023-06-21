2a3696507ffcb1e32d8a363bba09f061a70af05a
Firas Naarani <firas.narani.1999@gmail.com>
2023-06-21 11:04:36 +0300

    init

---
+++ src/main/java/com/KLA/Orbotech/OrbotechApplication.java
@@ -0,0 +1,13 @@
+package com.KLA.Orbotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbotechApplication.class, args);
+	}
+
+}
