75715cbad715f0df2b6abd658ea620bde4ef3905
Firas Naarani <firas.narani.1999@gmail.com>
2023-06-23 17:40:01 +0300

    add CLI

---
+++ Orbotech_CLI/src/main/java/com/KLA/Orbotech_CLI/OrbotechCliApplication.java
@@ -0,0 +1,13 @@
+package com.KLA.Orbotech_CLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbotechCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbotechCliApplication.class, args);
+	}
+
+}
---
+++ Orbotech_CLI/src/test/java/com/KLA/Orbotech_CLI/OrbotechCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KLA.Orbotech_CLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbotechCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
